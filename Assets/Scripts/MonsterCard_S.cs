cdd634db7d1628504dfcb3c998bfd917553adda8
Tyler Hernandez <tmh_dev@outlook.com>
2018-11-11 22:45:05 -0500

    initial commit

---
+++ Assets/Scripts/MonsterCard_S.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterCard_S : MonoBehaviour {
+
+    // Variables
+    public int strength; // or attack
+    // May or may not use armor/health
+    public int health;
+    public int armor;
+    public int goldReturn;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
