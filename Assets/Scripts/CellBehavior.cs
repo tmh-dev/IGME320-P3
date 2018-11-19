7a070befcb3e109e3baa58465fd144d88946c13b
seank0h <seankoh754@gmail.com>
2018-11-18 23:12:46 -0500

    Tilemap Created

---
+++ Assets/Scripts/CellBehavior.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CellBehavior : MonoBehaviour
+{
+    string tier;
+    string id;
+
+    int rng;
+
+    bool ifTraversal;
+
+
+    private void Awake()
+    {
+        
+    }
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
