6c58472ce4585119fba41a25f0c6439a4da39cd6
Tyler Hernandez <tmh_dev@outlook.com>
2018-11-26 00:48:37 -0500

    initial branch commit

---
+++ Assets/Scripts/New Scripts/ButtonManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonManager : MonoBehaviour {
+    public Button eventButton;
+    public GameObject eventHierarchy;
+
+	// Use this for initialization
+	void Start () {
+        eventButton.onClick.AddListener(OpenEvent);
+	}
+	
+	void OpenEvent()
+    {
+        eventHierarchy.GetComponent<Image>().enabled = true;
+    }
+}
